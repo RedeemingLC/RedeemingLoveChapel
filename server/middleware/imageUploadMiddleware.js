7956b0a425111969a2d896dfc6dc2d8fb9c714c3
RedeemingLC <redeeminglc@gmail.com>
2026-08-28 18:08:46 +0100

    Increase manual upload limits

--- server/middleware/imageUploadMiddleware.js
+++ server/middleware/imageUploadMiddleware.js
@@ -37,7 +37,7 @@ const imageUpload = multer({
   storage,
   fileFilter,
   limits: {
-    fileSize: 2 * 1024 * 1024, // 2MB
+    fileSize: 10 * 1024 * 1024, // 10MB
   },
 });
 
--- server/middleware/pdfUploadMiddleware.js
+++ server/middleware/pdfUploadMiddleware.js
@@ -35,7 +35,7 @@ const pdfUpload = multer({
   storage,
   fileFilter,
   limits: {
-    fileSize: 5 * 1024 * 1024, // 5MB
+    fileSize: 10 * 1024 * 1024, // 10MB
   },
 });
 
